6b6e15469e189f2d2b60cf510022d18ca88d55c6
Alptekin <alptekin.ocakdan1@ogr.sakarya.edu.tr>
2022-10-14 10:41:00 +0300

    Kodlama.io 2th day codes.

---
+++ EnginDemirog/Kodlama.io/Donguler/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //array
+            string[] kurslar = new string[]
+            {
+                "Yazılım Gelliştirme Kampı",
+                "Programlamaya başlamak için temel kurs",
+                "Java"
+            };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+        }
+    }
+}
--- EnginDemirog/Kodlama.io/KampIntro/Program.cs
+++ EnginDemirog/Kodlama.io/KampIntro/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 3200;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");   
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+            if (sistemeGirisYapmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+            else
+            {
+                Console.WriteLine("Selamun Aleykum");
+            }
+        }
+    }
+}
+
