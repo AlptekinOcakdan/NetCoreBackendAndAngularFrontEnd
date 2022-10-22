fdca708f4bb85a32241d5229f52e327d388e682b
Alptekin <alptekin.ocakdan1@ogr.sakarya.edu.tr>
2022-10-22 12:30:31 +0300

    Kodlama.IO Day 4

---
+++ EnginDemirog/Kodlama.io/KampIntro/Collections/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValueAndReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[]
+            {
+
+            };
+
+            List<string> names2 = new List<string>{"Alptekin","Serbil","Fırat"};
+            Console.WriteLine(names2[0]);
+            Console.WriteLine(names2[1]);
+            Console.WriteLine(names2[2]);
+            names2.Add("Murat");
+            Console.WriteLine(names2[3]);
+        }
+    }
+}
---
+++ EnginDemirog/Kodlama.io/KampIntro/GenericsIntro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericsIntro // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>();
+            names.Add("Alptekin");
+            //Console.WriteLine(names.Length);
+            names.Add("Alptekin");
+            //Console.WriteLine(names.Length);
+            foreach (var namesItem in names.Items)
+            {
+                Console.WriteLine(namesItem);
+            }
+        }
+    }
+}
