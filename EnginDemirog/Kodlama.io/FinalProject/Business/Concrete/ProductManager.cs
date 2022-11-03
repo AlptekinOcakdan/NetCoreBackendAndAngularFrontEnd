907d5395ca4db481d16793861d20c0204995074b
Alptekin Ocakdan <alptekin.ocakdan@gmail.com>
2022-11-03 12:06:33 +0300

    Kodlama.IO Day 9

--- EnginDemirog/Kodlama.io/FinalProject/Business/Concrete/ProductManager.cs
+++ EnginDemirog/Kodlama.io/FinalProject/Business/Concrete/ProductManager.cs
@@ -7,10 +7,11 @@ using Business.Abstract;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
+using Entities.DTOs;
 
 namespace Business.Concrete
 {
-    public class ProductManager:IProductService
+    public class ProductManager : IProductService
     {
         IProductDal _productDal;
 
@@ -21,17 +22,22 @@ namespace Business.Concrete
 
         public List<Product> GetAll()
         {
-            return  _productDal.GetAll();
+            return _productDal.GetAll();
         }
 
         public List<Product> GetAllByCategoryId(int id)
         {
-            return _productDal.GetAll(p=>p.CategoryId==id);
+            return _productDal.GetAll(p => p.CategoryId == id);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
         {
             return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            return _productDal.GetProductDetails();
         }
     }
 }
