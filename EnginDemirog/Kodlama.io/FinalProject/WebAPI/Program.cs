6e8e3ac3567c882689b8cac166cb0a5d772bc025
Alptekin Ocakdan <alptekin.ocakdan@gmail.com>
2022-12-01 17:07:25 +0300

    Kodlama.IO Day 12

--- EnginDemirog/Kodlama.io/FinalProject/WebAPI/Program.cs
+++ EnginDemirog/Kodlama.io/FinalProject/WebAPI/Program.cs
@@ -1,7 +1,6 @@
-using Business.Abstract;
-using Business.Concrete;
-using DataAccess.Abstract;
-using DataAccess.Concrete.EntityFramework;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Business.DependencyResolvers.Autofac;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,9 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//Autofac, Ninject, CastleWindsor, StructureMap, LightInject, DryInject --> IoC Container
-builder.Services.AddSingleton<IProductService,ProductManager>();
-builder.Services.AddSingleton<IProductDal, EfProductDal>();
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(builder => { builder.RegisterModule(new AutofacBusinessModule()); });
+//Autofac, Postsharp, Ninject, CastleWindsor, StructureMap, LightInject, DryInject --> IoC Container
+//builder.Services.AddSingleton<IProductService,ProductManager>();
+//builder.Services.AddSingleton<IProductDal, EfProductDal>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
