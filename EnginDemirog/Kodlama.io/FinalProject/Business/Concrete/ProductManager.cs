87723ac7035e38e7bbd9a5437652cdcad82f23d7
Alptekin <alptekin.ocakdan@gmail.com>
2022-12-06 16:56:30 +0300

    Kodlama.IO Day 13

--- EnginDemirog/Kodlama.io/FinalProject/Business/Concrete/ProductManager.cs
+++ EnginDemirog/Kodlama.io/FinalProject/Business/Concrete/ProductManager.cs
@@ -1,7 +1,9 @@
 ﻿using Business.Abstract;
+using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -12,19 +14,22 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
+        ICategoryService _categoryService;
 
-        public ProductManager(IProductDal productDal)
+        public ProductManager(IProductDal productDal, ICategoryService categoryService)
         {
             _productDal = productDal;
+            _categoryService = categoryService;
         }
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour==6)
+            if (DateTime.Now.Hour == 6)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
-            return new SuccessDataResult<List<Product>> (_productDal.GetAll(),Messages.ProductsListed);
+
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
         }
 
         public IDataResult<List<Product>> GetAllByCategoryId(int id)
@@ -34,7 +39,8 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max)
         {
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
+            return new SuccessDataResult<List<Product>>(
+                _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
         }
 
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
@@ -44,14 +50,54 @@ namespace Business.Concrete
 
         public IDataResult<Product> GetById(int productId)
         {
-            return new SuccessDataResult<Product>(_productDal.Get(p =>p.ProductId==productId));
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
         }
 
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
+            IResult result=BusinessRules.Run(CheckIfProductCountOfCategoryCorrect(product.CategoryId),CheckIfCategoryLimitExceeded(),CheckIfProductNameExist(product.ProductName));
+            if (result!=null)
+            {
+                return result;
+            }
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
+        public IResult Update(Product product)                
+        {
+            throw new NotImplementedException();
+        }
+        private IResult CheckIfProductCountOfCategoryCorrect(int categoryId)
+        {
+            var result = _productDal.GetAll(p => p.CategoryId == categoryId).Count;
+            if (result >= 10)
+            {
+                return new ErrorResult(Messages.ProductCountOfCategoryError);
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfProductNameExist(string productName)
+        {
+            var result=_productDal.GetAll(p=>p.ProductName== productName).Any();
+            if (result)
+            {
+                return new ErrorResult(Messages.ProductNameAlreadyExist);
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfCategoryLimitExceeded()
+        {
+            var result = _categoryService.GetAll();
+            if (result.Data.Count>15)
+            {
+                return new ErrorResult(Messages.CheckIfCategoryLimitExceeded);
+            }
+            return new SuccessResult();
+        }
+
+        
     }
 }
--- EnginDemirog/Kodlama.io/FinalProject/Business/Constants/Messages.cs
+++ EnginDemirog/Kodlama.io/FinalProject/Business/Constants/Messages.cs
@@ -6,5 +6,8 @@
         public static string ProductNameInvalid = "Ürün adı geçersiz";
         public static string MaintenanceTime = "Sistem Bakımda";
         public static string ProductsListed = "Ürünler Listelendi";
+        internal static string ProductCountOfCategoryError="Kategoriye 10 üründen fazlası eklenemez.";
+        internal static string ProductNameAlreadyExist="Var olan bir ad eklenemez.";
+        internal static string CheckIfCategoryLimitExceeded="Kategori sayısı 15'ten fazla olmaz.";
     }
 }
